f001237a2beddee7b2a45ceeab64ea223850ca40
kareem shetta <kareemshetta8@gmail.com>
2022-08-24 22:39:06 +0200

    chat app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import'package:firebase_messaging/firebase_messaging.dart';
 
 import './screens/auth_screen.dart';
 
@@ -10,14 +11,60 @@ import './screens/chat_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
 
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  print('User granted permission: ${settings.authorizationStatus}');
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+ // await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
+
+
+
+
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  @override
+  void initState() {
+
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
+FirebaseMessaging.instance.subscribeToTopic('chat');
+
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(debugShowCheckedModeBanner: false,
@@ -33,13 +80,15 @@ class MyApp extends StatelessWidget {
               borderRadius: BorderRadius.circular(20),
             ),
           ),
-          textTheme: TextTheme().copyWith(headline1: TextStyle(color: Colors.white)),
+          textTheme: TextTheme().copyWith(
+              headline1: TextStyle(color: Colors.white)),
           colorScheme: ColorScheme.fromSwatch().copyWith(
             primary: Colors.pink,
             secondary: Colors.purpleAccent,
 
           )),
       home: StreamBuilder<User?>(
+       // authStateChanges() will get the new user when changing in users and will notifies stream
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (ctx, userDataSnapshot) {
           if (userDataSnapshot.hasData) {
